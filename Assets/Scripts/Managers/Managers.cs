509d7cb30ce78976202e8efa7dbd7469954c0c17
Knabin <ppoxx77@gmail.com>
2020-09-01 22:22:43 +0900

    20-09-01
    스크립트 추가
    유니티쨩 애셋 추가

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance;   // 유일성이 보장된다
+    public static Managers Instance { get { Init(); return s_instance; } }  // 유일한 매니저를 갖고 온다
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if(s_instance == null)
+        {
+            // 초기화
+            // 각각의 Managers가 선언되었다고 해도 이렇게 찾아서 사용한다면 원본(@Managers)의 Managers가 instance에 저장될 것임
+            GameObject go = GameObject.Find("@Managers");
+
+            // Scene에 GameObject인 @Managers가 없다면 새로 추가함
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+            // 새로운 Scene이 로드될 때 삭제되지 않게끔
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // GameObject (Player)
+        // Transform
+        // PlayerController (*)
+    void Update()
+    {
+        //transform.gameObject처럼 gameObject 가져올 수 있음!
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0.0f, 0.0f, 1.0f);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+        }
+    }
+}
