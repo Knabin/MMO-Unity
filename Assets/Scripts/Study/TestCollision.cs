594a512d23362b59c3dea603cade98c0c0c7ab7d
Knabin <ppoxx77@gmail.com>
2020-09-10 01:15:55 +0900

    20-09-09

--- Assets/Scripts/Study/TestCollision.cs
+++ Assets/Scripts/Study/TestCollision.cs
@@ -71,8 +71,11 @@ public class TestCollision : MonoBehaviour
 
             Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
+            // Monster인 경우에만 Raycast
+            //int mask = ((1 << 8) | (1 << 9));
+
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 100.0f))
+            if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Monster")))
             {
                 Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
             }
