5d4b4d34e815a8c8afed8fa60f6920490560d6d5
Knabin <ppoxx77@gmail.com>
2020-09-20 22:56:37 +0900

    20-09-20

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager
 {
@@ -13,6 +14,9 @@ public class InputManager
 
     public void OnUpdate()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
         if (Input.anyKey && KeyAction != null)
             KeyAction.Invoke();     // 키 액션 있었다고 알리기
 
--- Assets/Scripts/Utils/Define.cs
+++ Assets/Scripts/Utils/Define.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Define
 {
+    public enum UIEvent
+    {
+        Click,
+        Drag,
+    }
+
     public enum MouseEvent
     {
         Press,
         Click,
     }
+
     public enum CameraMode
     {
         QuarterView,
