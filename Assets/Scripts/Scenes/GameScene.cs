0576f7cf20de44d60efcf9ca8b6d750f405e46e5
Knabin <ppoxx77@gmail.com>
2020-09-24 04:15:28 +0900

    20-09-23

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -11,8 +11,11 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
+
+        Dictionary<int, Stat> dict = Managers.Data.StatDict;
     }
 
+ 
     public override void Clear()
     {
 
